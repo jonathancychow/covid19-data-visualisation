1fc4657fd7f14c26cc316210bc3b2268888c085f
Jonathan Chow <jonathan.chow@mclaren.com>
2020-11-14 18:38:18 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,12 +3,19 @@ from SignalProcessing import moving_average
 from uk_covid19 import Cov19API
 import math
 from plotly.subplots import make_subplots
-from visualise_region_data import get_nation_data
+from visualise_region_data import get_region_data
 from visualise_nation_data import get_nation_data
 
-fig = make_subplots(rows=3, cols=2, subplot_titles=('Confirmed Cases', 'Hospital Cases', 'New Admission'))
+fig = make_subplots(rows=3,
+                    cols=2,
+                    subplot_titles=('Confirmed Cases', 'Region Data - 7 Days Average', 'Hospital Cases', 'New Admission'),
+                    specs=[[{}, {"rowspan": 3}],
+                           [{}, None],
+                           [{}, None]]
+                    )
 
 nations = ['England', 'Wales', 'Scotland', 'Northern Ireland']
+# Column 1
 for this_nation in nations:
     newCases, cumCases, date, hospitalCases, newAdmission = get_nation_data(this_nation)
 
@@ -26,8 +33,21 @@ for this_nation in nations:
                   )
         row_count += 1
 
+# column 2
+area = ['Kingston Upon Thames','Richmond Upon Thames','Epsom and Ewell','Hackney and City of London']
+for this_area in area:
+    newCases, cumCases, date, df = get_region_data(this_area)
 
+    x = date
+    y = newCases
+    N = 7
+    y_mva = moving_average(df['newCasesByPublishDate'], N)
 
-
+    fig.add_trace(go.Scatter(x=df['date'][(math.ceil(N/2)):], y=y_mva,
+                            mode='lines',
+                            name=this_area),
+                            row = 1,
+                            col = 2
+                  )
 
 fig.show()
