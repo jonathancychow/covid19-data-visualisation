70f8bd53b518cb6290d9debc461588ef99d0f699
Jonathan Chow <jonathan.chow@mclaren.com>
2020-11-18 13:08:11 +0000

    Added country data to dashboard, updated img

--- src/dashboard.py
+++ src/dashboard.py
@@ -4,13 +4,14 @@ import math
 from plotly.subplots import make_subplots
 from src.fetch.visualise_region_data import get_region_data
 from src.fetch.visualise_nation_data import get_nation_data
+from src.fetch.visualise_country import get_country_data
 
 fig = make_subplots(rows=3,
                     cols=2,
-                    subplot_titles=('Confirmed Cases', 'Region Data - 7 Days Average', 'Hospital Cases', 'New Admission'),
-                    specs=[[{}, {"rowspan": 3}],
+                    subplot_titles=('England - Confirmed Cases', 'Country Data - 7 Days Average', 'England - Hospital Cases', 'England - New Admission','England Regional Data - 7 Days Average'),
+                    specs=[[{}, {"rowspan": 2}],
                            [{}, None],
-                           [{}, None]]
+                           [{}, {}]]
                     )
 
 nations = ['England']
@@ -33,6 +34,24 @@ for this_nation in nations:
         row_count += 1
 
 # column 2
+country = ['united-kingdom','spain', 'france','germany','korea-south']
+status='confirmed'
+    # fig = make_subplots(rows=2, cols=1,subplot_titles=('Confirmed Cases','Deaths'))
+
+for thisCountry in country:
+    date, cumulativeCase, dailyCase = get_country_data(thisCountry, status)
+
+    x = date
+    y= dailyCase
+    N = 7
+    y_mva = moving_average(dailyCase, N)
+
+    fig.add_trace(go.Scatter(x=date,y=y_mva,
+                                 mode='lines',
+                                 name=thisCountry),
+                                 row=1,
+                                 col=2)
+
 area = ['Kingston Upon Thames','Richmond Upon Thames','Epsom and Ewell','Hackney and City of London']
 for this_area in area:
     newCases, cumCases, date, df = get_region_data(this_area)
@@ -45,7 +64,7 @@ for this_area in area:
     fig.add_trace(go.Scatter(x=df['date'][(math.ceil(N/2)):], y=y_mva,
                             mode='lines',
                             name=this_area),
-                            row = 1,
+                            row = 3,
                             col = 2
                   )
 
