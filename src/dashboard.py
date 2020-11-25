73e9a01bbf39b3910c293e4783362ee9823f1462
Jonathan Chow <jonathan.chow@mclaren.com>
2020-11-25 13:04:02 +0000

    More cosmetic changes

--- src/dashboard.py
+++ src/dashboard.py
@@ -9,37 +9,48 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-fig = make_subplots(rows=3,
-                    cols=2,
-                    subplot_titles=('England - Confirmed Cases', 'Country Data - 7 Days Average', 'England - Hospital Cases', 'England - New Admission','England Regional Data - 7 Days Average'),
-                    specs=[[{}, {"rowspan": 2}],
-                           [{}, None],
-                           [{}, {}]]
+fig1 = make_subplots(rows=1,
+                    cols=3,
+                    subplot_titles=('Confirmed Cases', 'Hospital Cases', 'New Admission')
                     )
 
 nations = ['England']
-# Column 1
+# Row 1
 for this_nation in nations:
     newCases, cumCases, date, hospitalCases, newAdmission = get_nation_data(this_nation)
 
     x = date
     data = [newCases,hospitalCases, newAdmission]
 
-    row_count = 1
+    col_count = 1
     for this_data in data:
 
-        fig.add_trace(go.Scatter(x = x, y = this_data,
+        fig1.add_trace(go.Scatter(x = x, y = this_data,
                                 mode = 'lines',
                                 name = this_nation),
-                                row=row_count,
-                                col=1
+                                row=1,
+                                col=col_count
                   )
-        row_count += 1
+        col_count += 1
 
-# column 2
+fig1.update_layout(template="plotly_white",
+                   showlegend=False,
+                   title={
+                       'text': "England Summary",
+                       'x': 0.5,
+                       'xanchor': 'center',
+                       'yanchor': 'top'}
+                   )
+
+# Row 2
+fig2 = make_subplots(rows=1,
+                     cols=3,
+                     subplot_titles=('Country - Confimred Cases', 'England Regional - Confirmed Cases'),
+                     specs=[[{"colspan": 2}, None, {}]]
+                     )
+
 country = ['united-kingdom','spain', 'france','germany','korea-south']
 status='confirmed'
-    # fig = make_subplots(rows=2, cols=1,subplot_titles=('Confirmed Cases','Deaths'))
 
 for thisCountry in country:
     date, cumulativeCase, dailyCase = get_country_data(thisCountry, status)
@@ -49,11 +60,11 @@ for thisCountry in country:
     N = 7
     y_mva = moving_average(dailyCase, N)
 
-    fig.add_trace(go.Scatter(x=date,y=y_mva,
+    fig2.add_trace(go.Scatter(x=date,y=y_mva,
                                  mode='lines',
                                  name=thisCountry),
                                  row=1,
-                                 col=2)
+                                 col=1)
 
 area = ['Kingston Upon Thames','Richmond Upon Thames','Epsom and Ewell','Hackney and City of London']
 for this_area in area:
@@ -64,27 +75,34 @@ for this_area in area:
     N = 7
     y_mva = moving_average(df['newCasesByPublishDate'], N)
 
-    fig.add_trace(go.Scatter(x=df['date'][(math.ceil(N/2)):], y=y_mva,
+    fig2.add_trace(go.Scatter(x=df['date'][(math.ceil(N/2)):], y=y_mva,
                             mode='lines',
                             name=this_area),
-                            row = 3,
-                            col = 2
+                            row = 1,
+                            col = 3
                   )
 
-fig.update_layout(template = "plotly_white",
-                    title = {
-                        'text': "COVID 19 Cases Dashboard",
-                        'x': 0.5,
-                        'xanchor': 'center',
-                        'yanchor': 'top'}
-                  )
 
-app = dash.Dash()
+fig2.update_layout(template="plotly_white",
+                   title={
+                       'text': "COVID 19 Cases Dashboard",
+                       'x': 0.5,
+                       'xanchor': 'center',
+                       'yanchor': 'top'}
+                   )
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(external_stylesheets=external_stylesheets)
 server = app.server
 
-app.layout = html.Div([
-    dcc.Graph(figure=fig,style={'height': '90vh'})
-])
+app.layout = html.Div(
+    children=[
+# html.H1(children='COVID 19 Cases Dashboard'),
+        html.Div([
+            dcc.Graph(figure=fig2, style={'height': '45vh'})]),
+        html.Div([
+            dcc.Graph(figure=fig1, style={'height': '45vh'})])
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
